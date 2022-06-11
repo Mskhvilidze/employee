30b53e2852d7fb7ad1fa199302e027ee800909d3
Mskhvilidze <33086639+Mskhvilidze@users.noreply.github.com>
2022-06-11 20:19:52 +0200

    pagination

--- src/main/resources/static/js/jq.js
+++ src/main/resources/static/js/jq.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function (newChild, refChild) {
     //Information of Article
     $(".inf").click(function (e) {
         $("#myModal").css("display", "block");
@@ -17,7 +17,7 @@ $(document).ready(function () {
     });
 
     //Article Delete
-    $(".deleteArt").click(function (e){
+    $(".deleteArt").click(function (e) {
         let $target = $(e.target);
         let nickname = $target.attr("data-id");
         alert("Test: " + nickname);
@@ -30,15 +30,41 @@ $(document).ready(function () {
     for (let i = 0; i < tr.length; i++) {
         arrayTr.push(tr[i]);
     }
-    $("#selectTopic").change(function (e){
-        /*$('table > tbody  > tr').each(function(index, tr) {
-        tbody.appendChild(tr)
-        });*/
-
+    /* for (let i = 0; i < 12; i++) {
+         tbody.appendChild(arrayTr[i]);
+     }*/
+    $("#selectTopic").change(function (e) {
         let value = $("#selectTopic option:selected").val();
-        tbody.innerHTML = "";
+        if (value == "NO SET") {
+            ('table > tbody  > tr').each(function (index, tr) {
+                tbody.appendChild(tr);
+            });
+        } else
+            tbody.innerHTML = "";
         for (let i = 0; i < value; i++) {
             tbody.appendChild(arrayTr[i]);
         }
     });
+
+    if (arrayTr.length > 2) {
+        const nofPage = arrayTr.length / 2;
+        for (let i = 0; i <= nofPage; i++) {
+            let li = document.createElement("li");
+            li.className = "ulLi";
+            let a = document.createElement("a");
+            a.href = "#";
+            a.setAttribute("data-page", "" +i);
+            li.appendChild(a);
+            a.innerHTML = "" + i;
+            $(".pagination").append(li);
+        }
+        let li = document.createElement("li");
+        let a = document.createElement("a");
+        li.className = "next nextLi ulLi";
+        a.href = "#";
+        a.id = "next";
+        li.appendChild(a);
+        a.innerHTML = "&#155";
+        $(".pagination").append(li);
+    }
 });
